5fa3b5284342948bfbe06ef7575a2e8fc2f7ea44
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-06 01:19:55 +0530

    cities migration, model created

---
+++ database/migrations/2021_06_05_193305_create_cities_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('city_name');
+            $table->string('city_code');
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+
+            $table->unsignedBigInteger('country_id');
+            $table->foreign('country_id')->references('id')
+                    ->on('countries')
+                    ->onDelete('cascade')
+                    ->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
