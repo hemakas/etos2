fa139f539e7d6148f986eb5899456d6402c174c4
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-08 10:54:31 +0530

    currencies migration, model created

---
+++ database/migrations/2021_06_07_070727_create_currencies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('iso_code');
+            $table->string('currency_name');
+            $table->integer('country');
+            $table->string('symbol');
+            $table->integer('active')->nullable();
+            $table->float('roe')->nullable();
+            $table->integer('default')->nullable();
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
