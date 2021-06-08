3226b57febd1ec5915e4e5ec44ab68e90a26e745
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-08 12:41:30 +0530

    excursions migration, model created

---
+++ database/migrations/2021_06_08_070950_create_excursions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExcursionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('excursions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('city_name');
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('excursions');
+    }
+}
