5b5a71ccc86320a7bb046bbd826902ce94883122
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-10 17:07:49 +0530

    meal_plans migration, model created

---
+++ database/migrations/2021_06_10_113329_create_meal_plans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealPlansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meal_plans', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('alias');
+            $table->tinyInteger('is_active');
+            $table->tinyInteger('show_on_voucher');
+            $table->text('description');
+            $table->string('meal_code');
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal_plans');
+    }
+}
