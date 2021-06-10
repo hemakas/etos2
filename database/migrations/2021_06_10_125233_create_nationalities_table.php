c8b3574ca82ae6eb2aaa83c13c12cbedf2b86ccd
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-10 18:31:58 +0530

    nationalities migration, model created

---
+++ database/migrations/2021_06_10_125233_create_nationalities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNationalitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nationalities', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nationality');
+            $table->string('nationality_code');
+           
+            $table->unsignedBigInteger('national_id');
+            $table->foreign('national_id')->references('id')
+                ->on('nationals')
+                ->onDelete('cascade');
+
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nationalities');
+    }
+}
