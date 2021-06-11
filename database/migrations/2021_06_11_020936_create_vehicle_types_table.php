364972171acf0390af5c4c94c22c914a5d65475a
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-11 07:53:40 +0530

    vehicle_types migration, model created

---
+++ database/migrations/2021_06_11_020936_create_vehicle_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('vehicle_name');
+            $table->integer('driver_charge')->default(0);
+            $table->integer('rate')->default(0);
+            $table->tinyInteger('is_active')->default(0);
+            $table->tinyInteger('divisible_factors')->default(0);
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_types');
+    }
+}
