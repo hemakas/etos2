acc13ce771a8b836779f00051a90638bb531b70d
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-11 22:55:20 +0530

    model_has_roles migration, model created

---
+++ database/migrations/2021_06_11_172355_create_model_has_roles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelHasRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('model_has_roles', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')
+                ->on('roles')
+                ->onDelete('cascade');
+            
+            $table->string('model_type');
+            $table->integer('model_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('model_has_roles');
+    }
+}
