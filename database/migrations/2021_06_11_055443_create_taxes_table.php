e4a1fbca418f0c578fb575627e77d383e2fe78d0
Hemaka Ranasinghe <hemaka.tropusi@gmail.com>
2021-06-11 11:26:11 +0530

    taxes migration, model created

---
+++ database/migrations/2021_06_11_055443_create_taxes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tax_type');
+            $table->float('tax_value', 8, 2)->default(0.0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taxes');
+    }
+}
